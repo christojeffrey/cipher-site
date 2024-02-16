e94c258fd6f3c4d2c0c993023140160b48952c1b
Wesly Giovano <weslygio@yahoo.com>
2024-02-16 15:51:43 +0700

    add playfair cipher endpoint

--- ror/app/controllers/application_controller.rb
+++ ror/app/controllers/application_controller.rb
@@ -3,13 +3,15 @@ require_relative './../../../vercel/functions/functions'
 require_relative './../../../vercel/functions/a-vigenere-standard-cipher'
 require_relative './../../../vercel/functions/b-vigenere-autokey-cipher'
 
+require_relative './../../../vercel/functions/d-playfair-cipher'
+
 class ApplicationController < ActionController::API
 
   # EXAMPLES
   def testing
     render json: { name: 'hello' }
   end
-  
+
   def hello
       render json: hello_controller("world")
   end
@@ -28,14 +30,21 @@ class ApplicationController < ActionController::API
   def vigenere_standard_cipher
     # get body
     body = request.body.read
-    
+
     render json: vigenere_standard_cipher_controller(body)
   end
   def vigenere_autokey_cipher
     # get body
     body = request.body.read
-    
+
     render json: vigenere_autokey_cipher_controller(body)
+  end
+
+  def playfair_cipher
+    # get body
+    body = request.body.read
+
+    render json: playfair_cipher_controller(body)
   end
 
 end
