43b719a3f1ba2e843e7527892fd2afc0a68f1cad
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-02-18 17:29:01 +0700

    unfinished: add heading

--- solid-front/src/components/input-box.tsx
+++ solid-front/src/components/input-box.tsx
@@ -4,7 +4,8 @@ import { Component } from "solid-js";
 export const InputBox: Component = () => {
   const [_, setInput] = inputSignal;
   return (
-    <div class="border-2 w-1/3">
+    <div class="w-1/3 bg-white rounded-2xl p-2">
+      <Heading>Input</Heading>
       <input
         type="text"
         class="border-2"
@@ -14,6 +15,10 @@ export const InputBox: Component = () => {
       />
     </div>
   );
+};
+
+const Heading: Component<{ children: any }> = (props: { children: any }) => {
+  return <h1 class="text-2xl">{props.children}</h1>;
 };
 
 export default InputBox;
