0b0996df0bb59aa4292cdd975b3eca2921eae63a
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-02-19 14:17:34 +0700

    testing: cors on deployed

--- vercel/api/hello.rb
+++ vercel/api/hello.rb
@@ -3,6 +3,7 @@ require_relative  './../functions/functions'
 
 Handler = Proc.new do |request, response|
     name = request.query['name'] || 'World'
+
    
     response.status = 200
     response['Content-Type'] = 'application/json'
