65f010803fb8eef5504a49ef240904cb3efac88c
Wesly Giovano <weslygio@yahoo.com>
2024-02-22 07:19:24 +0700

    allow cors on enigma

--- vercel/api/cipher/enigma.rb
+++ vercel/api/cipher/enigma.rb
@@ -1,6 +1,9 @@
 require_relative  './../../functions/bonus-enigma-cipher'
 
 Handler = Proc.new do |request, response|
+  # handle cors
+  response['Access-Control-Allow-Origin'] = '*'
+  response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
 
   response.status = 200
   response['Content-Type'] = 'application/json'
